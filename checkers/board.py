778f855ab567ed2fb4759771c428665ef9555add
jabloemker <95373714+jabloemker@users.noreply.github.com>
2023-01-22 22:05:49 -0700

    setup board class to draw checkerboard pattern setup piece class for maintaining pieces

---
+++ checkers/board.py
@@ -0,0 +1,17 @@
+import pygame
+from .constants import BLACK, RED, ROWS, COLS, SQUARE_SIZE
+
+
+class Board:
+    def __init__(self):
+        self.board = []
+        self.selected_piece = None
+        self.red_left = self.white_left = 12
+        self.red_kings = self.white_kings = 0
+
+    def draw_board_pattern(self, win):
+        win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row % 2, COLS, 2):
+                pygame.draw.rect(win, RED, (row*SQUARE_SIZE,
+                                 col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
