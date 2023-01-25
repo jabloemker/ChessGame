e9a5b79a37e43e0ed6bbc4778685cd23390dff43
jabloemker <95373714+jabloemker@users.noreply.github.com>
2023-01-24 20:08:27 -0700

    start setting up game control logic

--- checkers/constants.py
+++ checkers/constants.py
@@ -10,4 +10,5 @@ BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 GREY = (128, 128, 128)
 
-CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (45, 25))
+CROWN = pygame.transform.scale(pygame.image.load(
+    'checkers/assets/crown.png'), (45, 25))
